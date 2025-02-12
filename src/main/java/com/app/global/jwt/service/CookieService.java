c502a6bd8321caa8c7025b8523b9fae8ee30e1a6
jean202 <hb13pud731@gmail.com>
2025-02-12 09:53:26 +0900

    [SCRUM-68] Cookie -> ResponseCookie로 변경

--- src/main/java/com/app/global/jwt/service/CookieService.java
+++ src/main/java/com/app/global/jwt/service/CookieService.java
@@ -1,8 +1,8 @@
 package com.app.global.jwt.service;
 
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,21 +11,27 @@ public class CookieService {
 
     // HttpOnly 쿠키 생성 메서드
     public void addHttpOnlyCookie(HttpServletResponse response, String name, String value, long expirationTime) {
-        Cookie cookie = new Cookie(name, value);
-        cookie.setHttpOnly(true); // JavaScript 접근 방지
-        cookie.setSecure(true);  // HTTPS 전용 설정
-        cookie.setPath("/");     // 도메인 전체에 적용
-        cookie.setMaxAge((int) expirationTime); // ms -> s 변환 // ms -> s 변환
-        response.addCookie(cookie);
+        ResponseCookie refreshTokenCookie = ResponseCookie.from(name, value)
+                .httpOnly(true)    // JavaScript에서 접근 불가능
+                .secure(true)
+                .sameSite("None")  // 크로스 사이트 요청을 허용하려면 "None"
+                .path("/")         // 도메인 전체에 적용
+                .maxAge(expirationTime) // 쿠키 만료 시간 설정 (초 단위)
+                .build();
+
+        response.addHeader("Set-Cookie", refreshTokenCookie.toString());
     }
 
     // 쿠키 삭제 메서드
     public void removeCookie(HttpServletResponse response, String name) {
-        Cookie cookie = new Cookie(name, null);
-        cookie.setHttpOnly(true);
-        cookie.setSecure(true);
-        cookie.setPath("/");
-        cookie.setMaxAge(0); // 즉시 만료
-        response.addCookie(cookie);
+        ResponseCookie refreshTokenCookie = ResponseCookie.from(name, "")
+                .httpOnly(true)    // JavaScript에서 접근 불가능
+                .secure(true)
+                .sameSite("None")  // 크로스 사이트 요청을 허용하려면 "None"
+                .path("/")         // 도메인 전체에 적용
+                .maxAge(0)         // 즉시 만료 (삭제)
+                .build();
+
+        response.addHeader("Set-Cookie", refreshTokenCookie.toString());
     }
 }
